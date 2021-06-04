399bc49454b8d91933407037143e6d21e0d0e051
weibin <15209272743@163.com>
2021-06-04 10:51:18 +0800

    modify java333

--- Dockerfile.java
+++ Dockerfile.java
@@ -3,4 +3,4 @@ WORKDIR /java-demo
 ADD ./java-demo .
 RUN mvn -B -ntp -Dmaven.test.failure.ignore verify
 
-#
+# "store_artifacts": "/java-demo/target/simple-maven-project-with-tests-1.0-SNAPSHOT.jar"
