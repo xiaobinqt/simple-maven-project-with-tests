9c2246f2de34f05356ce05c3fc7ddb9d65c95f49
weibin <15209272743@163.com>
2021-06-04 09:47:23 +0800

    modify java

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,5 +1,6 @@
-FROM openjdk:7
-COPY . /usr/src/myapp
-WORKDIR /usr/src/myapp
-RUN java -version
+FROM maven:3.8.1-openjdk-8-slim
+WORKDIR /simple-maven-project-with-tests
+ADD . .
+RUN mvn -B -ntp -Dmaven.test.failure.ignore verify
 
+# "store_artifacts": "/simple-maven-project-with-tests/target/simple-maven-project-with-tests-1.0-SNAPSHOT.jar"
